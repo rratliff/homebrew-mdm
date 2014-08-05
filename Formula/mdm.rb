a6c139f4b9e41a6bd6fa5201daebd7fdcac5dd0d
John Shimek <jshimek@code42.com>
2014-08-05 09:37:11 -0500

    Updated to mdm v2.18.0.

--- Formula/mdm.rb
+++ Formula/mdm.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Mdm < Formula
   homepage "https://github.com/polydawn/mdm"
-  url "https://github.com/mdm-releases/mdm-releases/blob/master/v2.17.4/mdm?raw=true", :using => :nounzip
-  sha1 "d919f5329902245cb296e123f4582f5539701fbb"
-  version "2.17.4"
+  url "https://github.com/mdm-releases/mdm-releases/blob/master/v2.18.0/mdm?raw=true", :using => :nounzip
+  sha1 "47f57b41e4cadffa08bc9ab8631645009d67adf5"
+  version "2.18.0"
 
   def install
     bin.install Dir["mdm"]
